31148144ec31c9870aa7871b32bb46ace3f52d72
Saad <saadabbou89@gmail.com>
2021-08-22 00:01:36 +0100

    redux test begin

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -5,6 +5,7 @@ import homeimg from "../assets/home.png";
 const Home = () => {
   return (
     <div className="home-container">
+      <h2>REdux test</h2>
       <img src={homeimg} alt="home-photo" width="800px" />
       <div>
         <a href="/add" className="home-page-link">
